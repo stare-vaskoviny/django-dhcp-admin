9108bf1966f62ddce5ca8eca117d2860fb6c79ee
Vaclav Klecanda <vencax77@gmail.com>
2012-10-02 19:15:09 +0200

    view for adding PC (via script from windoze)

---
+++ dhcp_admin/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import patterns, url
+
+from . import views
+
+urlpatterns = patterns('',
+    url(r'^(?P<mac>[-:\w]+)/(?P<name>\w+)/$', views.AddMacView.as_view(), name='dhcp_admin_add_mac'),
+)
--- dhcp_admin/views.py
+++ dhcp_admin/views.py
@@ -1,1 +1,29 @@
-# Create your views here.
+
+from django.views.generic.base import View
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseServerError
+import string
+
+from .models import PC
+
+class AddMacView(View):
+    
+    net_part = getattr(settings, 'NET_PART', '10.0.0')
+    
+    def get(self, request, *args, **kwargs):
+        mac, name = kwargs['mac'], kwargs['name'].lower()
+        cleaned = ''
+        for l in mac:
+            if l in string.hexdigits:
+                cleaned += l
+        if not PC.objects.filter(mac=mac).exists():
+            for i in range(10, 249):
+                try:
+                    p = PC(name=name, mac=cleaned, 
+                           ip='%s.%i' % (self.net_part, i))
+                    p.save()
+                    return HttpResponse('OK')
+                except Exception:
+                    pass
+        return HttpResponseServerError('FULL')
+        
