0ef38760e0d1adc20878221b5eead13e12fe2927
Vaclav Klecanda <vencax77@gmail.com>
2012-10-03 19:17:31 +0200

    textarea widget for mac

--- dhcp_admin/admin.py
+++ dhcp_admin/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin, messages
+from django.forms.widgets import Textarea
 from django.utils.translation import ugettext_lazy as _
 
 from .forms import MyPCAdminForm
 from .models import PC
+
 
 def start_pcs(modeladmin, request, queryset):
     for m in queryset:
@@ -15,5 +17,10 @@ class PCAdmin(admin.ModelAdmin):
     search_fields = ['name', 'ip', 'mac']
     form = MyPCAdminForm
     actions = [start_pcs]
+    
+    def formfield_for_dbfield(self, db_field, **kwargs):
+        if db_field.name == 'mac':
+            kwargs['widget'] = Textarea
+        return super(PCAdmin, self).formfield_for_dbfield(db_field, **kwargs)
 
 admin.site.register(PC, PCAdmin)
